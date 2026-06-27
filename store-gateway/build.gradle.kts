612715f43b14fc32b8fd074388064d7f49f45c50
EduardoMeloDeOliveira <eduardo.moliveira.dev@hotmail.com>
2026-06-27 01:17:16 -0300

    feat: commit inicial do core do projeto gateway

--- store-gateway/build.gradle.kts
+++ store-gateway/build.gradle.kts
@@ -1,20 +1,48 @@
 plugins {
-    kotlin("jvm") version "2.3.21"
+    id("org.springframework.boot") version "3.3.2"
+    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("plugin.spring") version "1.9.24"
+    kotlin("jvm") version "1.9.24"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.shop"
+version = "0.0.1"
+java.sourceCompatibility = JavaVersion.VERSION_17
 
-repositories {
-    mavenCentral()
-}
+extra["springCloudVersion"] = "2023.0.3"
+
+repositories { mavenCentral() }
+
+
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
 }
 
 kotlin {
     jvmToolchain(17)
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${extra["springCloudVersion"]}")
+    }
 }
 
 tasks.test {
