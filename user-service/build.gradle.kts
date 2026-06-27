e1d3a36034366184e77c043fc63e64910384be21
EduardoMeloDeOliveira <eduardo.moliveira.dev@hotmail.com>
2026-06-27 19:33:30 -0300

    feat: configura todas as dependencias do projeto

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -1,23 +1,36 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.study.shop"
+version = "0.0.1"
+
+java { toolchain { languageVersion = JavaLanguageVersion.of(17) } }
+
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.security:spring-security-crypto")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")}
+
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
 }
 
-kotlin {
-    jvmToolchain(17)
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
+tasks.withType<Test> { useJUnitPlatform() }
